d143a8e3ec026057d6960939b80ea0a22d9a9d7c
Federico Navarrete <fanmixco@gmail.com>
2020-09-03 09:40:05 +0200

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+export default () => {
+    return (
+        <div>
+	      I'm some other page
+	      <Link to="/">Go back to home page!</Link>
+        </div>
+    );
+};
